d94461fc6f2f180ffd36eca2068bc871142039ef
jenstiza <jennifermariehoskins@gmail.com>
2022-07-04 01:34:03 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 
 
 /*----- app's state (variables) -----*/
-
+let board;  
+let turn;
 
 
 /*----- cached element references -----*/
@@ -15,3 +16,26 @@
 
 
 /*----- functions -----*/
+init();
+
+function init( ) {
+board = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+];
+turn = 1;
+render();
+}
+
+function render( ) {
+    board.forEach(function (colArr, colIdx) {
+        colArr.forEach(function(cellVal, rowIdx) {
+            const cellEl = document.getElementById(`c${colIdx}r${rowIdx}`);
+       
+    });
+        
+    });
+}
