1b9f430dd766367711f6087aadc7da0ab451382f
jenstiza <jennifermariehoskins@gmail.com>
2022-07-07 20:11:07 -0400

    pseudo code and read me update

---
+++ wireframe/pseudocode.js
@@ -0,0 +1,25 @@
+/*----- constants -----*/
+//  colors for the discs the players use and the board slots
+
+/*----- app's state (variables) -----*/
+// variable for the board itself
+// variable for whose turn it is
+// variable for if the game is in play or not
+// variable for winner
+
+/*----- cached element references -----*/
+// marker elements - the triangles at the top of each column
+// the element to grab the play again button
+// the element for the message "player 1 wins" "player 2 wins" "it's a tie!"
+
+/*----- event listeners -----*/
+//need to listen for clicks on the triangle markers
+// need to listen for clicks on the play again button to reset board
+
+/*----- functions -----*/
+
+// need an init function with the fresh board
+// need a render function
+//need a renderMessages and renderMarkers function
+// function for wins horizontal/vertical/diagonal
+//gameStatus function
